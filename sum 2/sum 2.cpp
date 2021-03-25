eb598845c7de9ae1f66b6c3092e5735b9c458800
MariaIvanova01 <mimi200801@abv.bg>
2021-03-25 17:09:43 +0200

    Logic for addition of two integers implemented

---
+++ sum 2/sum 2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, m, t;
+    cout << " n = ";
+    cin >> n;
+    cout << " m = ";
+    cin >> m;
+    t = n + m;
+    cout << n + m  << endl;
+
+
+}
